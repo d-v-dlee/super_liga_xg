8d7095ab7b2b33d59a2e0784ef6aa99a56f4d2e5
David Lee <davidlee327@gmail.com>
2018-11-30 13:14:16 -0800

    create functions to create df to see player prob from different spots

--- create_shot_coordinates.py
+++ create_shot_coordinates.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from dataframe_cleaner import shot_distance_angle
 
 def coord_maker(y_start, y_end, y_unit, x_start, x_end, x_unit):
     y_co = list(range(y_start, y_end + y_unit, y_unit))
@@ -16,4 +17,15 @@ def coord_maker(y_start, y_end, y_unit, x_start, x_end, x_unit):
 def coord_table(y_start, y_end, y_unit, x_start, x_end, x_unit):
     x_list, y_list = coord_maker(y_start, y_end, y_unit, x_start, x_end, x_unit)
     return pd.DataFrame( {'shot_coord_x1': x_list, 'shot_coord_y1': y_list})
+
+def distance_pred(df, player_id):
+    df['assisted_shot'] = 0
+    df['is_penalty_attempt'] = 0
+    df_2 = shot_distance_angle(df)
+    df_final = df_2.drop(columns=['shot_coord_x1', 'shot_coord_y1'])
+    df_final['player_id'] = player_id
+    return df_final
+    
+
+
 
