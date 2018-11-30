ca2631440afccbe39719788ba4b36b1d3cb23760
David Lee <davidlee327@gmail.com>
2018-11-30 09:37:21 -0800

    write function to make test and holdout split

--- model_prep.py
+++ model_prep.py
@@ -14,7 +14,6 @@ def manual_train_split(df, train_sample=80):
     holdout_test, train = manual_train_split(shots_df)
     returns: game_ids to hold out/predict on, list to train on
     """
-    rf_columns = ['player_id', 'shot_distance', 'shot_angle', 'assisted_shot', 'is_penalty_attempt']
 
     possible_games = list(df['game_id'].unique())
     game_numbers = len(possible_games)
@@ -25,11 +24,33 @@ def manual_train_split(df, train_sample=80):
     for i in games_to_sample:
         games_to_train_on.append(possible_games[i])
     
-    for game in games_to_sample:
+    for game in games_to_train_on:
         if game in possible_games:
             possible_games.remove(game)
     
     return possible_games, games_to_train_on
+
+def manual_test_split(possible_games, test_sample=50):
+    """input: possible_games which is game_ids minus the game_ids used for training
+    output: list of games_ids to predict and remaining holdout set
     
+    first for loop: takes random sample, then corresponding game_ids in list (by position)
+    second for loop: removes those sample games from the possible_games list, leaving final holdout_list"""
+
+    games_left = len(possible_games)
+
+    games_to_sample = np.random.choice(games_left, test_sample)
+
+    games_to_predict = []
+    for i in games_to_sample:
+        games_to_predict.append(possible_games[i])
+
+    for game in games_to_predict:
+        if game in possible_games:
+            possible_games.remove(game)
     
+        holdout_games = possible_games.copy()
+    
+    return holdout_games, games_to_predict
+        
     
