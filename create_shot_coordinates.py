555d980988f3641af324a966bc658e281b53e345
David Lee <davidlee327@gmail.com>
2018-12-04 10:22:11 -0800

    make changes so that shot data is generated without player_id

--- create_shot_coordinates.py
+++ create_shot_coordinates.py
@@ -28,27 +28,29 @@ def coord_table(y_start, y_end, y_unit, x_start, x_end, x_unit):
     x_list, y_list = coord_maker(y_start, y_end, y_unit, x_start, x_end, x_unit)
     return pd.DataFrame( {'shot_coord_x1': x_list, 'shot_coord_y1': y_list})
 
-def distance_pred(df, player_id, assisted=0):
+def distance_pred(df, assisted=0):
+    """turn shot coordinates into distance and angle"""
     df['assisted_shot'] = assisted
     df['is_penalty_attempt'] = 0
     df_2 = shot_distance_angle(df)
     df_final = df_2.drop(columns=['shot_coord_x1', 'shot_coord_y1'])
-    df_final['player_id'] = player_id
+    # df_final['player_id'] = player_id
     return df_final
     
 
-def shot_probability_player(player_id):
+def shot_probability_player():
+    """create shot tables for the six, eighteen, etc by yardage"""
     six_yard_shot = coord_table(-10, 10, 2, 2, 6, 2)
     eighteen_yard_shot = coord_table(-10, 10, 2, 8, 18, 2)
     beyond_eighteen_yard_shot = coord_table(-22, 22, 2, 20, 30, 2)
     left_six_yard_shot = coord_table(12, 22, 2, 3, 18, 3)
     right_six_yard_shot = coord_table(-12, -22, -2, 3, 18, 3)
 
-    six = distance_pred(six_yard_shot, player_id)
-    eighteen = distance_pred(eighteen_yard_shot, player_id)
-    eighteen_plus = distance_pred(beyond_eighteen_yard_shot, player_id)
-    left_box = distance_pred(left_six_yard_shot, player_id)
-    right_box = distance_pred(right_six_yard_shot, player_id)
+    six = distance_pred(six_yard_shot)
+    eighteen = distance_pred(eighteen_yard_shot)
+    eighteen_plus = distance_pred(beyond_eighteen_yard_shot)
+    left_box = distance_pred(left_six_yard_shot)
+    right_box = distance_pred(right_six_yard_shot)
 
     return six, eighteen, eighteen_plus, left_box, right_box
 
